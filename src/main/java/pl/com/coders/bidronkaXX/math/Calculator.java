307401e94e69fe3f748eec74cff73adf029b6ed7
pszczepkowski <p.szczepkowski87@gmail.com>
2023-09-18 20:36:00 +0200

    ZAD_3 add many

--- src/main/java/pl/com/coders/bidronkaXX/math/Calculator.java
+++ src/main/java/pl/com/coders/bidronkaXX/math/Calculator.java
@@ -1,8 +1,36 @@
 package pl.com.coders.bidronkaXX.math;
 
+import java.util.List;
+
 public class Calculator {
 
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int add(List<Integer> list) {
+        int sum = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            sum = sum + list.get(i);
+        }
+
+//        for (Integer i : list) {
+//            sum = sum + i;
+//        }
+        return sum;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public float divide(float a, float b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Nie dzieliemy przez 0");
+        }
+        return a / b;
+    }
+
+
 }
--- src/test/java/pl/com/coders/bidronkaXX/math/CalculatorTest.java
+++ src/test/java/pl/com/coders/bidronkaXX/math/CalculatorTest.java
@@ -2,6 +2,9 @@ package pl.com.coders.bidronkaXX.math;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -20,5 +23,47 @@ class CalculatorTest {
         int add = calculator.add(0, 1);
 
         assertEquals(1, add);
+    }
+
+    @Test
+    void multiply() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.multiply(2, 2);
+
+        assertEquals(4, sum);
+    }
+
+    @Test
+    void divide() {
+        Calculator calculator = new Calculator();
+        float divide = calculator.divide(10, 3);
+
+        assertEquals(3.3333332538604736, divide);
+    }
+
+    @Test
+    void testAdd() {
+        List<Integer> ints = new ArrayList<>();
+        ints.add(1);
+        ints.add(2);
+        ints.add(3);
+
+        Calculator calculator = new Calculator();
+        int add = calculator.add(ints);
+
+        assertEquals(6, add);
+    }
+
+    @Test
+    void testAddMany() {
+        List<Integer> ints = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            ints.add(i);
+        }
+
+        Calculator calculator = new Calculator();
+        int add = calculator.add(ints);
+
+        assertEquals(4950, add);
     }
 }
